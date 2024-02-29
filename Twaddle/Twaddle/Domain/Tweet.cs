9864008286fec138e972c577050124f853360b26
Rasmus Sandbæk <sandbxk@gmail.com>
2024-02-29 11:43:32 +0100

    Create domain model

---
+++ Twaddle/Twaddle/Domain/Tweet.cs
@@ -0,0 +1,11 @@
+﻿
+public class Tweet
+{
+    public ulong Id { get; set; }
+    public string? Text { get; set; }
+    public string? Author { get; set; }
+    public IEnumerable<Tweet> Replies { get; set; }
+    public int Likes { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
---
+++ UserService/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿
+public class User
+{
+    public ulong Id { get; set; }
+    public string? Username { get; set; }   
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Bio { get; set; }
+    public string? ProfilePicture { get; set; }
+    public IEnumerable<User> Following { get; set; }
+    public IEnumerable<User> Followers { get; set; }
+}
