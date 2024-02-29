a2e77d1610f03a160350066acdb9419063b2c21b
Rasmus Sandbæk <sandbxk@gmail.com>
2024-02-29 12:13:43 +0100

    add tweet domain

---
+++ TweetService/Domain/Tweet.cs
@@ -0,0 +1,12 @@
+﻿namespace TweetService.Domain;
+
+public class Tweet
+{
+    public ulong Id { get; set; }
+    public string? Content { get; set; }
+    public string? Author { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public IEnumerable<ulong> Likes { get; set; }
+    public IEnumerable<Tweet> Replies { get; set; }
+    public Tweet? ReplyTo { get; set; }
+}
