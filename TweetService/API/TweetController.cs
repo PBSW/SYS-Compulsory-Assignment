522a18b376c9d22cd1b13ab81773ccd13f2f35a8
Rasmus Sandbæk <sandbxk@gmail.com>
2024-02-29 12:29:37 +0100

    Add API controller for tweets

---
+++ TweetService/API/TweetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TweetService.API;
+
+[ApiController]
+[Route("api/tweet")]
+public class TweetController : ControllerBase
+{
+    TweetController()
+    {
+    }
+    
+    
+}
---
+++ UserService/API/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.API;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : ControllerBase
+{
+    public UserController()
+    {
+    }
+
+    [HttpGet]
+    public User GetUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public User UpdateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public User DeleteUser([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public User Login(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public User Register(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet]
+    [Route("{id}/following")]
+    public IEnumerable<User> GetFollowers([FromRoute] int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    [Route("{id}/follow/{followId}")]
+    public User FollowUser([FromRoute] int id, [FromRoute] int followId)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost]
+    [Route("{id}/unfollow/{unfollowId}")]
+    public User UnfollowUser([FromRoute] int id, [FromRoute] int unfollowId)
+    {
+        throw new NotImplementedException();
+    }
+}
